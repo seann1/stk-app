410710c0c889ff7924c9b2b8c8f56953adec7f06
Sean Niesen <seanniesen@gmail.com>
2015-03-21 20:02:14 -0700

    Initial Commit

---
+++ Mandolin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mandolin
+//
+//  Created by Sean Niesen on 3/21/15.
+//  Copyright (c) 2015 Sean Niesen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mandolin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mandolin
+//
+//  Created by Sean Niesen on 3/21/15.
+//  Copyright (c) 2015 Sean Niesen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
